552a757a39f2411354ca94ff9b937cccca68af8a
Abdellah Sabiri <abdellah.sabiri.1995@gmail.com>
2019-08-01 19:56:39 +0100

    Character Plugin

---
+++ CharactersManager/Character.js
@@ -0,0 +1,68 @@
+import { sumOfElementPoints } from './utils';
+
+/**
+ * The stats data of the character
+ */
+class Character {
+
+	constructor(username){
+		this.username = username;
+		this.level = {};
+		this.elements = {};
+		this.AP = 0;
+		this.MP = 0;
+		this.lifePoints = {};
+		this.kamas = 0;
+		this.statsPoints = 0;
+		this.spellsPoints = 0;
+		this.spells = {};
+	}
+
+	updateStat(stats){
+
+		//Updating Kamas
+		const {kamas} = stats;
+		this.kamas = kamas;
+
+		//Update State & Spell points
+		const {spellsPoints, statsPoints} = stats;
+		this.spellsPoints = spellsPoints;
+		this.statsPoints = statsPoints;
+
+		//Updating Elements Stats
+		const {vitality, strength, agility, chance, intelligence, wisdom} = stats;
+		this.elements = {
+			vitality: sumOfElementPoints(vitality),
+			strength: sumOfElementPoints(strength),
+			agility: sumOfElementPoints(agility),
+			chance: sumOfElementPoints(chance),
+			intelligence: sumOfElementPoints(intelligence),
+			wisdom: sumOfElementPoints(wisdom)
+		}
+
+		//Updating AP & MP
+		const {actionPointsCurrent, movementPointsCurrent} = stats;
+		this.AP = actionPointsCurrent;
+		this.MP = movementPointsCurrent;
+
+
+		//Updating life points
+		const {lifePoints, maxLifePoints} = stats;
+		this.lifePoints = {
+			current: lifePoints,
+			max: maxLifePoints
+		}
+
+		//Update level & xp
+		//TODO: Get level from xp table
+		const {experience, experienceLevelFloor, experienceNextLevelFloor} = stats;
+		this.level = {
+			currentXp: experience,
+			nextLevelXp: experienceNextLevelFloor,
+			currentLevelXpFloor: experienceLevelFloor
+		}
+
+	}
+}
+
+export default CharacterStats;
